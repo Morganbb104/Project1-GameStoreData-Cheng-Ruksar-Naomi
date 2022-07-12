ddc1201d6cd041778161753e0ec17f3c25b26a52
Naomi Lounsbury <naomilounsbury@hotmail.com>
2022-07-12 12:43:05 -0700

    intellij initial commit

---
+++ M4-Summative-Cheng-Chien-Ruksar-Naomi/src/test/java/com/example/M4SummativeChengChienRuksarNaomi/M4SummativeChengChienRuksarNaomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.M4SummativeChengChienRuksarNaomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4SummativeChengChienRuksarNaomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
