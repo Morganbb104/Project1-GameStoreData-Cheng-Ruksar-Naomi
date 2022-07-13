190255d00ca064ae0e246265e1ac03787c09b756
scouring pad <hcc780921@gmail.com>
2022-07-13 07:25:32 -0700

    properties SQL added, models games added

--- M4-Summative-Cheng-Chien-Ruksar-Naomi/src/main/java/com/example/M4SummativeChengChienRuksarNaomi/models/Games.java
+++ M4-Summative-Cheng-Chien-Ruksar-Naomi/src/main/java/com/example/M4SummativeChengChienRuksarNaomi/models/Games.java
@@ -1,5 +1,14 @@
 package com.example.M4SummativeChengChienRuksarNaomi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Table(name = "game")
 public class Games {
     private String name;
+    private int id;
 }
