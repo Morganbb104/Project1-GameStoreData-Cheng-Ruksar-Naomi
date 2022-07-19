122c018b9e75155e7d62b0e84e6a15987c193d3d
scouring pad <hcc780921@gmail.com>
2022-07-18 19:09:22 -0700

    invoice repo test findbyId

--- M4-Summative-Cheng-Chien-Ruksar-Naomi/src/test/java/com/example/M4SummativeChengChienRuksarNaomi/repository/InvoiceRepositoryTest.java
+++ M4-Summative-Cheng-Chien-Ruksar-Naomi/src/test/java/com/example/M4SummativeChengChienRuksarNaomi/repository/InvoiceRepositoryTest.java
@@ -50,7 +50,7 @@ public class InvoiceRepositoryTest {
 
         invoice = invoiceRepository.save(invoice);
         Optional<Invoice> invoice1 = invoiceRepository.findById(invoice.getId());
-        assertEquals(invoice1.get().getId(),invoice);
+        assertEquals(invoice1.get().getId(),invoice.getId());
 
         invoice1 = invoiceRepository.findById(invoice.getId());
         assertFalse(invoice1.isPresent());
